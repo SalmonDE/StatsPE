a3c83032b1ae8bf1b6638ac4a28fb6d639c487c1
SalmonDE <salmonde@salmonde.de>
2016-07-20 11:15:07 +0200

    Fix some bugs

--- src/StatsPE/UpdaterTask.php
+++ src/StatsPE/UpdaterTask.php
@@ -16,20 +16,18 @@ class UpdaterTask extends AsyncTask{
 
 	public function onRun(){
 		$dfile = Utils::getURL($this->url);
-		if(file_exists('plugins\StatsPE_v'.$this->version.'.phar')){
 		    if(md5($dfile) == $this->md5hash){
-				file_put_contents('plugins\StatsPE_v'.$this->newversion.'.phar', $dfile);
-			    unlink('plugins\StatsPE_v'.$this->version.'.phar');
 				if(file_exists('plugins\StatsPE_v'.$this->version.'.phar')){
-					file_put_contents('StatsPEUpdaterError.log', 'Could not delete old phar file!');
-				}elseif(!file_exists('plugins/StatsPE_v'.$this->newversion.'.phar')){
-					file_put_contents('StatsPEUpdaterError.log', 'Could not download new phar file!');
+			        unlink('plugins\StatsPE_v'.$this->version.'.phar');
+			    }else{
+					file_put_contents('StatsPEUpdaterError.log', 'Old StatsPE phar not found! Please make sure to name the StatsPE phar file like this: StatsPE_v'.$this->version.'.phar or if you use the source code disable the Auto Updater to prevent errors');
 				}
-		   }else{
+				file_put_contents('plugins\StatsPE_v'.$this->newversion.'.phar', $dfile);
+			    if(!file_exists('plugins/StatsPE_v'.$this->newversion.'.phar')){
+					file_put_contents('StatsPEUpdaterError.log', 'Could not download new phar file!');
+			    }
+		    }else{
 			    file_put_contents('StatsPEUpdaterError.log', 'md5 hash of the downloaded file was not correct');
-		   }
-		}else{
-			file_put_contents('StatsPEUpdaterError.log', 'Old StatsPE phar not found! Please make sure to name the StatsPE phar file like this: StatsPE_v'.$this->version.'.phar or if you use the source code disable the Auto Updater to prevent errors');
-		}
+		    }
 	}
 }
