a3dccd8e241d94ba04e6462319c66a3ca26eaf5a
SalmonDE <salmonde@salmonde.de>
2016-07-28 22:29:26 +0500

    This will really fix #5

--- src/StatsPE/UpdaterTask.php
+++ src/StatsPE/UpdaterTask.php
@@ -19,13 +19,13 @@ class UpdaterTask extends AsyncTask{
 	public function onRun(){
 		$dfile = Utils::getURL($this->url);
 		    if(md5($dfile) == $this->md5hash){
-				if(file_exists("plugins\StatsPE_v".$this->version.'.phar')){
-			        unlink("plugins\StatsPE_v".$this->version.'.phar');
+				if(file_exists("plugins/StatsPE_v".$this->version.'.phar')){
+			        unlink("plugins/StatsPE_v".$this->version.'.phar');
 			    }else{
 					file_put_contents('StatsPEUpdaterError.log', 'Old StatsPE phar not found! Please make sure to name the StatsPE phar file like this: StatsPE_v'.$this->version.'.phar or if you use the source code disable the Auto Updater to prevent errors');
 				}
-				file_put_contents("plugins\StatsPE_v".$this->newversion.'.phar', $dfile);
-			    if(!file_exists("plugins\StatsPE_v".$this->newversion.'.phar')){
+				file_put_contents("plugins/StatsPE_v".$this->newversion.'.phar', $dfile);
+			    if(!file_exists("plugins/StatsPE_v".$this->newversion.'.phar')){
 					file_put_contents('StatsPEUpdaterError.log', 'Could not download new phar file!');
 			    }
 		    }else{
