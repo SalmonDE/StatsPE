0200b1975f87a8980caef31f409e0ff79a8569ae
SalmonDE <salmonde@salmonde.de>
2016-08-15 13:26:57 +0200

    Fix another crash

--- src/StatsPE/Updater/CheckVersionTask.php
+++ src/StatsPE/Updater/CheckVersionTask.php
@@ -24,28 +24,14 @@ class CheckVersionTask extends AsyncTask
 		if(!$nversion){
 			//$this->getLogger()->warning(TF::RED.'Checking for Update Failed: Empty Response');
 		}else{
-		    if($cversion == $nversion){
-			    //$this->owner->getLogger()->info(TF::GREEN.'Your '.$this->owner->getDescription()->getName().' version ('.TF::AQUA.$cversion.TF::GREEN.') is up to date! :)');
-		    }else{
-			    //$this->owner->getLogger()->notice(TF::RED.TF::BOLD.'Update available for '.$this->owner->getDescription()->getName().'!'."\n".TF::RED.'Current version: '.$cversion."\n".TF::GREEN.TF::BOLD.'Newest version: '.$nversion);
-			    if($this->autoupdate){
-				    //$this->owner->getLogger()->warning(TF::GREEN.'Will run an update for '.$this->owner->getDescription()->getName()."($cversion)".' to version: '.$nversion);
-					$this->setResult(true);
-				}/*else{
-				    if($this->owner->isPhar()){
-				        $this->owner->getLogger()->info(TF::AQUA.'Please set "Auto-Update" to "true" to automatically update the plugin!');
-				    }else{
-					    $this->owner->getLogger()->info(TF::AQUA.TF::BOLD.'It seems that you are not using a StatsPE phar. It still updates, but the source folder will not be removed. Please delete the file manually to prevent errors.');
-				    }
-			    }*/
-		    }
+        $this->setResult($nversion);
 		}
 	}
 
 	public function onCompletion(Server $server){
-		if($this->getResult()){
-			$server->broadcastTip(TF::GREEN.'Running an update for '.$this->owner->getDescription()->getName()." ($cversion)".' to version: '.$nversion);
-			$this->owner->update($nversion);
+		if(!$this->cversion == $this->getResult()){
+			$server->broadcastMessage(TF::GREEN.'Running an update for '.$this->name." ($this->cversion)".' to version: '.$this->nversion);
+			StatsPE::update($this->nversion);
 		}
 	}
 }
