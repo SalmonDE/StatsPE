06df0cf339eab16cfff4662a12bf94dfe0377e20
SalmonDE <salmonde@salmonde.de>
2016-08-24 17:58:04 +0200

    Updating mysql data on join works now
    @AppleMinerHD es ist noch nicht komplett fertig, aber ich hoffe ich
    schaffe es noch heute. :)

--- src/StatsPE/Tasks/SaveDataTask.php
+++ src/StatsPE/Tasks/SaveDataTask.php
@@ -22,36 +22,23 @@ class SaveDataTask extends AsyncTask
   public function onRun(){
       $connection = mysqli_connect($this->mysql['host'], $this->mysql['user'], $this->mysql['password'], $this->mysql['database']);
       if($connection){
-          mysqli_query($connection, "
-          IF EXISTS (
-   SELECT
-      *
-   FROM
-      Stats
-   WHERE
-      PlayerName=$this->player
-) BEGIN
-UPDATE
-   Stats
-SET
-   $this->stat=$this->data
-WHERE
-   PlayerName=$this->player
-END
-ELSE BEGIN
-   INSERT
-   INTO
-      Stats
-      (PlayerName, XBoxAuthenticated, FirstJoin, LastJoin, JoinCount, KillCount, DeathCount, KickCount, OnlineTime, BlocksBreaked, BlocksPlaced, ChatMessages, FishCount, EnterBedCount, EatCount, CraftCount)
-   VALUES
-      ($this->player, 'false', date($this->timeformat), date($this->timeformat), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-END");
+          $poccurence = mysqli_num_rows(mysqli_query($connection, "SELECT * FROM Stats WHERE PlayerName = '$this->player'"));
+          $date = date($this->timeformat);
+          if($poccurence == 1){
+              mysqli_query($connection, "UPDATE Stats SET $this->stat = '".$this->data."' WHERE PlayerName = '".$this->player."'");
+          }elseif($poccurence > 1){
+              $this->setResult('Player '.$this->player.' is occuring more than once in Table Stats of the Database '.$this->mysql['database']);
+          }else{
+              mysqli_query($connection, "INSERT INTO Stats (PlayerName, XBoxAuthenticated, FirstJoin, LastJoin, JoinCount, KillCount, DeathCount, KickCount, OnlineTime, BlocksBreaked, BlocksPlaced, ChatMessages, FishCount, EnterBedCount, EatCount, CraftCount) VALUES ('$this->player', 'false', '$date', '$date', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)");
+          }
           if(mysqli_error($connection)){
               $this->setResult(mysqli_error($connection));
           }else{
               $this->setResult(false);
           }
-    }
+      }else{
+              $this->setResult('Could not connect to Database');
+      }
   }
 
   public function onCompletion(Server $server){
