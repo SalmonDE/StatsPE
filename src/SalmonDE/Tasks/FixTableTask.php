0bacdfd146fea28d803d85190ee59ec3fbb3e4e4
SalmonDE <salmonde@salmonde.de>
2016-08-27 01:38:26 +0200

    Finish /fixtable

--- src/SalmonDE/Tasks/FixTableTask.php
+++ src/SalmonDE/Tasks/FixTableTask.php
@@ -25,20 +25,118 @@ class FixTableTask extends AsyncTask
             $connection = @mysqli_connect($this->mysql['host'], $this->mysql['user'], $this->mysql['password'], $this->mysql['database']);
             if($connection){
                 $stats = [
-                    '1' => [
-                        'Name' => 'Online',
-                        'Default' => $this->yes
+                    '1' =>[
+                        'Name' => 'PlayerName',
+                        'Datatype' => 'VARCHAR(16)',
+                        'Extra' => 'NOT NULL UNIQUE'
                     ],
                     '2' => [
+                        'Name' => 'Online',
+                        'Datatype' => 'VARCHAR10',
+                        'Extra' => "NOT NULL DEFAULT '".$this->yes."'"
+                    ],
+                    '3' => [
+                        'Name' => 'ClientID',
+                        'Datatype' => 'VARCHAR(30)',
+                        'Extra' => 'NOT NULL'
+                    ],
+                    '4' => [
+                        'Name' => 'UUID',
+                        'Datatype' => 'VARCHAR(30)',
+                        'Extra' => 'NOT NULL'
+                    ],
+                    '5' => [
                         'Name' => 'XBoxAuthenticated',
-                        'Default' => $this->no
+                        'Datatype' => 'VARCHAR(10)',
+                        'Extra' => "NOT NULL DEFAULT '".$this->no."'"
+                    ],
+                    '6' => [
+                        'Name' => 'LastIP',
+                        'Datatype' => 'VARCHAR(15)',
+                        'Extra' => 'NOT NULL'
+                    ],
+                    '7' => [
+                        'Name' => 'FirstJoin',
+                        'Datatype' => 'VARCHAR(30)',
+                        'Extra' => 'NOT NULL'
+                    ],
+                    '8' => [
+                        'Name' => 'LastJoin',
+                        'Datatype' => 'VARCHAR(30)',
+                        'Extra' => 'NOT NULL'
+                    ],
+                    '9' => [
+                        'Name' => 'JoinCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '10' => [
+                        'Name' => 'KillCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '11' => [
+                        'Name' => 'DeathCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '12' => [
+                        'Name' => 'KickCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '13' => [
+                        'Name' => 'OnlineTime',
+                        'Datatype' => 'VARCHAR(30)',
+                        'Extra' => "DEFAULT '0'"
+                    ],
+                    '14' => [
+                        'Name' => 'BlocksBreaked',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '15' => [
+                        'Name' => 'BlocksPlaced',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '16' => [
+                        'Name' => 'ChatMessages',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '17' => [
+                        'Name' => 'FishCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '18' => [
+                        'Name' => 'EnterBedCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '19' => [
+                        'Name' => 'EatCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '20' => [
+                        'Name' => 'CraftCount',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
+                    ],
+                    '21' => [
+                        'Name' => 'DroppedItems',
+                        'Datatype' => 'INT(255)',
+                        'Extra' => "UNSIGNED DEFAULT 0"
                     ]
                 ];
                 foreach($stats as $column){
                     $name = $column['Name'];
-                    $default = $column['Default'];
-                    mysqli_query($connection, "ALTER TABLE Stats ADD $name VARCHAR(10) NOT NULL DEFAULT '$default'");
-                    mysqli_query($connection, "ALTER TABLE Stats CHANGE '$name' '$name' VARCHAR(10) SET DEFAULT '$default'");
+                    $datatype = $column['Datatype'];
+                    $extra = $column['Extra'];
+                    mysqli_query($connection, "ALTER TABLE Stats ADD $name $datatype $extra");
+                    mysqli_query($connection, "ALTER TABLE Stats CHANGE '$name' '$name' $datatype SET $extra");
                 }
             }else{
                 $this->setResult($this->lang['MySQL']['ConnectFailure'].mysqli_error($connection));
