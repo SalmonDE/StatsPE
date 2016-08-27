8470f7be363390ec4e5afbfd8bb1cdb0750e272f
SalmonDE <salmonde@salmonde.de>
2016-08-28 01:01:30 +0200

    Trying to fix syntaxchecker.php

--- syntaxcheck.php
+++ syntaxcheck.php
@@ -1,7 +1,7 @@
 <?php
-$base = glob('plugins/StatsPE-src/*.php');
-$updater = glob('plugins/StatsPE-src/Updater/*.php');
-$tasks = glob('plugins/StatsPE-src/Tasks/*.php');
+$base = glob("plugins/StatsPE-src/*.php");
+$updater = glob("plugins/StatsPE-src/Updater/*.php");
+$tasks = glob("plugins/StatsPE-src/Tasks/*.php");
 $files = array_merge($base, $updater, $tasks);
 var_dump($base);
 var_dump($updater);
@@ -10,6 +10,6 @@ var_dump($files);
 foreach($files as $file){
     exec("php -l $file", $output);
 }
-//foreach($output as $line){
+foreach($output as $line){
     //echo($line);
-//}
+}
