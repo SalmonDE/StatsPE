220070137d6ea3934ebed5555ccb7d8e95b3b565
SalmonDE <salmonde@salmonde.de>
2016-08-28 00:55:26 +0200

    debug

--- syntaxcheck.php
+++ syntaxcheck.php
@@ -2,10 +2,14 @@
 $base = glob('plugins/StatsPE-src/*.php');
 $updater = glob('plugins/StatsPE-src/Updater/*.php');
 $tasks = glob('plugins/StatsPE-src/Tasks/*.php');
-$phpfiles = array_merge($base, $updater, $tasks);
-foreach($phpfiles as $file){
+$files = array_merge($base, $updater, $tasks);
+var_dump($base);
+var_dump($updater);
+var_dump($tasks);
+var_dump($files);
+foreach($files as $file){
     exec("php -l $file", $output);
 }
-foreach($output as $line){
-    echo($line);
-}
+//foreach($output as $line){
+    //echo($line);
+//}
