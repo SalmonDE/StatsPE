07eb534bcf128cd257701fe3c642fb082b4f3463
SalmonDE <salmonde@salmonde.de>
2016-08-28 01:06:06 +0200

    Fix syntaxcheck.php

--- syntaxcheck.php
+++ syntaxcheck.php
@@ -1,15 +1,11 @@
 <?php
-$base = glob("plugins/StatsPE-src/*.php");
-$updater = glob("plugins/StatsPE-src/Updater/*.php");
-$tasks = glob("plugins/StatsPE-src/Tasks/*.php");
+$base = glob("plugins/StatsPE-src/src/*.php");
+$updater = glob("plugins/StatsPE-src/src/Updater/*.php");
+$tasks = glob("plugins/StatsPE-src/src/Tasks/*.php");
 $files = array_merge($base, $updater, $tasks);
-var_dump($base);
-var_dump($updater);
-var_dump($tasks);
-var_dump($files);
 foreach($files as $file){
     exec("php -l $file", $output);
 }
 foreach($output as $line){
-    //echo($line);
+    echo($line);
 }
