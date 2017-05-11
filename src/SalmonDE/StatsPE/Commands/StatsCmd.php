c668d481bde28c68f5c5cfd5a5e2fa7bdb2ac54d
SalmonDE <salmonde@salmonde.de>
2017-05-11 13:12:30 +0200

    Merge command classes with executors

--- src/SalmonDE/StatsPE/Commands/StatsCmd.php
+++ src/SalmonDE/StatsPE/Commands/StatsCmd.php
@@ -1,7 +1,10 @@
 <?php
 namespace SalmonDE\StatsPE\Commands;
 
-class StatsCmd extends \pocketmine\command\PluginCommand
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+
+class StatsCmd extends \pocketmine\command\PluginCommand implements \pocketmine\command\CommandExecutor
 {
 
     public function __construct(\SalmonDE\StatsPE\Base $owner){
@@ -9,6 +12,59 @@ class StatsCmd extends \pocketmine\command\PluginCommand
         $this->setPermission('statspe.cmd.stats');
         $this->setDescription($owner->getMessage('commands.stats.description'));
         $this->setUsage($owner->getMessage('commands.stats.usage'));
-        $this->setExecutor(new StatsCmdExecutor());
+        $this->setExecutor($this);
+    }
+
+    public function onCommand(\pocketmine\command\CommandSender $sender, \pocketmine\command\Command $cmd, $label, array $args){
+        if(!isset($args[0])){
+            if(!$sender instanceof Player){
+                return false;
+            }
+            $args[0] = $sender->getName();
+        }
+
+        if(is_array($data = $this->getPlugin()->getDataProvider()->getAllData($args[0]))){
+            $text = str_replace('{value}', $data['Username'], $this->getPlugin()->getMessage('general.header'));
+            foreach($this->getPlugin()->getDataProvider()->getEntries() as $entry){
+                if($sender->hasPermission('statspe.entry.'.$entry->getName())){
+                    switch($entry->getName()){
+                        case 'FirstJoin':
+                            $p = $sender->getServer()->getOfflinePlayer($args[0]);
+                            $value = date($this->getPlugin()->getConfig()->get('Date-Format'), $p->getFirstPlayed() / 1000);
+                            break;
+
+                        case 'LastJoin':
+                            $p = $sender->getServer()->getOfflinePlayer($args[0]);
+                            $value = date($this->getPlugin()->getConfig()->get('Date-Format'), $p->getLastPlayed() / 1000);
+                            break;
+
+                        case 'OnlineTime':
+                            $seconds = $data['OnlineTime'];
+                            if(($p = $sender->getServer()->getPlayerExact($data['Username'])) instanceof Player){
+                                $seconds += round(time() - ($p->getLastPlayed() / 1000));
+                            }
+
+                            $value = \SalmonDE\StatsPE\Utils::getPeriodFromSeconds($seconds);
+                            break;
+
+                        case 'K/D':
+                            $value = \SalmonDE\StatsPE\Utils::getKD($data['KillCount'], $data['DeathCount']);
+                            break;
+
+                        case 'Online':
+                            $value = $data['Online'] ? $this->getPlugin()->getMessage('commands.stats.true') : $this->getPlugin()->getMessage('commands.stats.false');
+                            break;
+
+                        default:
+                            $value = $data[$entry->getName()];
+                    }
+                    $text .= TF::RESET."\n".TF::AQUA.$entry->getName().': '.TF::GOLD.$value;
+                }
+            }
+            $sender->sendMessage($text);
+        }else{
+            $sender->sendMessage(TF::RED.str_replace('{player}', $args[0], $this->getPlugin()->getMessage('commands.stats.notFound')));
+        }
+        return true;
     }
 }
